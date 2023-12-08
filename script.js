ac93f73101b32baf0674bdcf4644a3550c1f2233
Durgesh Sharma <109064132+Durge15@users.noreply.github.com>
2023-12-08 15:44:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
-
+const scrollcontainer=document.getElementById("infi-list");
+let i=0;
+function AddoneItem(){
+	const li=document.createElement("li");
+	li.innerText=`item ${++i}`;
+	scrollcontainer.appendChild(li);
+}
+for(let i=0;i<10;i++)
+	{
+		AddoneItem();
+	}
+scrollcontainer.addEventListener(scroll , ()=>{
+	const MaxScrollHeight=scrollcontainer.scrollHeight-scrollcontainer.clientHeight;
+	let currentScrollTop=scrollcontainer.scrollTop;
+	let threshold=Math.abs(MaxScrollHeight-currentScrollTop);
+	if(threshold<=5){
+		for(let i=0;i<2;i++)
+			{
+				AddoneItem();
+			}
+	}
+})
